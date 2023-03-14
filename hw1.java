53c4fbeb6d4a507b1abde3242c0c5418d1b80b43
horrently <127292323+horrently@users.noreply.github.com>
2023-03-14 20:40:42 +0800

    Add files via upload

---
+++ hw1.java
@@ -0,0 +1,24 @@
+/**
+* Javadoc
+*
+* @author Dmitriy Cherenkov
+*/
+
+public class hw1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++){
+            if (i % 3 == 0 & i % 5 == 0) {
+                System.out.printf("FizzBuzz \n");
+            }
+            else if (i % 3 == 0) {
+                System.out.printf("Fizz \n");
+            }
+            else if (i % 5 == 0) {
+                System.out.printf("Buzz \n");
+            }
+            else {
+                System.out.println( i );
+            }
+        }
+    }
+}
